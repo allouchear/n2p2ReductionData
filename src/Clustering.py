f0df9041af28a842f55612898ae2282fd49eb98b
allouchear <77264549+allouchear@users.noreply.github.com>
2023-11-01 14:58:55 +0100

    add Scaling, Reduction of dimensions & data

--- src/Clustering.py
+++ src/Clustering.py
@@ -11,17 +11,29 @@ from sklearn.cluster import DBSCAN
 from sklearn.cluster import HDBSCAN
 from sklearn.neighbors import NearestNeighbors
 from kneed import KneeLocator
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
 
 def getArguments():
 	parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
 	parser.add_argument("--infile", type=str, default="functions.h5", help="G functions from h5 file, see buildGh5.py")
 	parser.add_argument("--outfile", type=str, default="numStructs.csv", help="output file : list of selected structures")
+	parser.add_argument("--outclustersfile", type=str, default="clusters.csv", help="output file containing strcutures number, Z and clusters number")
 	parser.add_argument("--method", type=str, default="KMeans", help="method : KMeans, DBSCAN or HDBSCAN")
 	parser.add_argument("--k", type=int, default=5, help=" k = number of clusters (5 = default)")
 	parser.add_argument("--p", type=float, default=0.20, help=" real : % of selected structures by cluster, default=0.20")
 	parser.add_argument("--seed", type=int, default=111, help=" seed, default 111. If <0 => random_state=None")
 	parser.add_argument("--eps", type=float, default=-0.01, help=" eps for DBSCAN, if esp <0=> kneed locater of distances from NearestNeighbors*eps. For HDBSCAN min_cluster_size=ndata*eps")
 	parser.add_argument("--minsample", type=int, default=2, help=" minsample: integer, see DBSCAN method")
+	parser.add_argument("--reddim", type=str, default="None", help="method : None or PCA")
+	parser.add_argument("--kr", type=float, default=1, help=" kr = number of reduced dimension. kr can be a real between 0 and 1.0")
+	parser.add_argument("--scaling", type=str, default="None", help="scaling : None, MinMax, Standard or MaxAbs")
+	parser.add_argument("--reddata", type=str, default="None", help="Reduce data method : None(default), MaxG, StdG")
+	parser.add_argument("--kreddata", type=int, default=10, help="Max number of G")
 	args = parser.parse_args()
 	return args
 
@@ -53,6 +65,99 @@ def getOptimialEpsDBSCAN(df, args):
 	print("optimalEps=",kneedle.knee_y,flush=True)
 	return kneedle.knee_y
 
+def makePipeLineScalerRed(args, n_components):
+	scaler = None
+	if args.scaling.upper()=="STANDARD":
+		scaler = StandardScaler()
+	elif args.scaling.upper()=="MINMAX":
+		scaler = MinMaxScaler()
+	elif args.scaling.upper()=="MAXABS":
+		scaler = MaxAbsScaler()
+	else:
+		scaler = None
+	reddim = None
+	if args.kr< n_components and args.reddim.upper()!="NONE":
+		n_components=args.kr
+		if abs(n_components-1)<1e-10:
+			n_components=1
+		print("n_comps = ", n_components)
+		if args.reddim.upper()=="PCA":
+			reddim = PCA(n_components=n_components)
+		else:
+			reddim = None
+
+	pipe = None
+	if scaler is not None and reddim is not None:
+		pipe = Pipeline([('scaler', scaler), ('reddim', reddim)])
+	elif scaler is not None:
+		pipe = Pipeline([('scaler', scaler)])
+	elif reddim is not None:
+		pipe = Pipeline([('reddim', reddim)])
+	return pipe
+
+def getndf(pipe, df):
+	ndf=df.drop(columns=['Z'])
+	if pipe is None:
+		return ndf, None
+	params = pipe.get_params()
+	lists, _ = zip(*params['steps'][:])
+	#print(params)
+	#print(pipe)
+	#print(list1)
+	if not ('scalar' in lists or 'reddim' in lists ):
+		ndf = ndf.to_numpy(dtype='float32')
+	return ndf, lists
+
+def transformScalerRed(df, args):
+	pipe = makePipeLineScalerRed(args, df.shape[1]-1) # to remove 'Z'
+	ndf,lists = getndf(pipe, df)
+	if lists is not None:
+		ndf = pipe.fit_transform(ndf)
+		if 'reddim' in lists:
+			print("dimension after reduction = ", pipe.named_steps['reddim'].n_components_, flush=True)
+		else:
+			print("dimension = ", df.shape[1], flush=True)
+	return ndf
+
+def reduceData(df, args):
+	if args.reddata.upper()=="NONE":
+		return df
+	if args.kreddata>=df.shape[0]:
+		return df
+		#print(df)
+	if args.reddata.upper()=="MAXG" :
+		print("Reduction of data using G with max value")
+		dfs=df.max().to_frame()[1:]
+	else:
+		print("Reduction of data using G with max STD")
+		dfs=df.std().to_frame()[1:]
+
+	#print(dfs)
+	nameColG=dfs.idxmax()[0]
+	print("\tnumColGMax=", nameColG)
+	df.sort_values(by=[nameColG], inplace=True, ascending=False)
+	ns=df.shape[0]
+	nsel=int(args.kreddata)
+	nstep=ns//nsel
+	listi=list(range(0,ns))
+	listi=listi[0::nstep]
+	if len(listi)>nsel:
+		nstep +=1
+		nsel=int(ns//nstep)
+		listi=list(range(0,ns))
+		listi=listi[0::nstep]
+	print("\tTotal number of G=",ns)
+	print("\tNumber of selected G=",nsel)
+	print("\tnStep=",nstep)
+	#print(len(listi))
+	#print(listi)
+	print("\tshape before reduction of data ",df.shape)
+	df = df.iloc[listi]
+	#print(df)
+	print("\tshape after reduction of data = ",df.shape)
+	return df
+
+
 def DBSCANClustering(store,args):
 	seed = args.seed
 	percentage = args.p
@@ -61,19 +166,23 @@ def DBSCANClustering(store,args):
 	sample = []
 	random.seed(seed)
 	print("Types = ",store.keys(),flush=True)
+	dfClusters=None
 	for key in store.keys(): 
 		df = store.get(key)
 		z=int(df.iloc[0,0])
 		print("-------------------------------------------------------------------------",flush=True)
 		print("Clustering of atoms with atomic number Z = ",z)
 		print("=====================================================",flush=True)
-		print("DataFrame shape = ",df.shape)
+		df = reduceData(df, args)
+		print("DataFrame shape = ",df.shape, flush=True)
+		print("Number of variables = ",df.shape[1]-1, flush=True)
 		#print("Columns = ",df.columns)
 		#print(df)
 		#maxG=df.max()
 		#print(maxG)
 		#maxG = float(max(maxG[1:-1]))
 		#print(maxG)
+		ndf = transformScalerRed(df, args)
 		if args.method.upper()=="HDBSCAN":
 			e = eps
 			if eps<0:
@@ -82,12 +191,18 @@ def DBSCANClustering(store,args):
 			if nc<2 :
 				nc=2
 			print("min_cluster_size = ",nc)
-			dbscan = HDBSCAN(min_cluster_size=nc).fit(df)
+			dbscan = HDBSCAN(min_cluster_size=nc).fit(ndf)
 		else:
-			e=getOptimialEpsDBSCAN(df,args)
-			dbscan = DBSCAN(eps=e, min_samples=minsample).fit(df)
+			e=getOptimialEpsDBSCAN(df,args) # 'Z' dropped in getOptimal
+			dbscan = DBSCAN(eps=e, min_samples=minsample).fit(ndf)
 		dbscan.fit(df)
 		df["predicted_cluster"] = dbscan.labels_
+
+		dfc=df[["Z","predicted_cluster"]]
+		if dfClusters is None:
+			dfClusters=dfc
+		else:
+			dfClusters=pd.concat([dfClusters,dfc])
 		k = len(set(dbscan.labels_ [dbscan.labels_ != -1])) # number of clusters without -1
 		#print(dbscan.labels_)
 		km1 = len(dbscan.labels_ [dbscan.labels_ == -1]) # number of outliers
@@ -107,7 +222,7 @@ def DBSCANClustering(store,args):
 	#print(len(sample))
 	sample = set(sample)
 	sample = sorted(sample)
-	return sample
+	return sample, dfClusters
 
 def KMeansClustering(store,args):
 	percentage = args.p
@@ -118,14 +233,26 @@ def KMeansClustering(store,args):
 	sample = []
 	random.seed(seed)
 	print("Types = ",store.keys())
+	dfClusters=None
 	for key in store.keys(): 
 		df = store.get(key)
 		z=int(df.iloc[0,0])
+		print("-------------------------------------------------------------------------",flush=True)
 		print("Clustering of atoms with atomic number Z = ",z,flush=True)
+		print("=====================================================",flush=True)
+		df = reduceData(df, args)
+		print("DataFrame shape = ",df.shape, flush=True)
+		print("Number of variables = ",df.shape[1]-1, flush=True)
+		ndf = transformScalerRed(df, args)
 		kmeans = KMeans(n_clusters=k,random_state=seed,n_init='auto')
 		#kmeans = KMeans(n_clusters=k,init='random',random_state=seed)
-		kmeans.fit(df)
+		kmeans.fit(ndf)
 		df["predicted_cluster"] = kmeans.labels_
+		dfc=df[["Z","predicted_cluster"]]
+		if dfClusters is None:
+			dfClusters=dfc
+		else:
+			dfClusters=pd.concat([dfClusters,dfc])
 
 		for i in range(k):
 			indexes = df.index[df['predicted_cluster'] == i].tolist()
@@ -135,7 +262,7 @@ def KMeansClustering(store,args):
 	#print(len(sample))
 	sample = set(sample)
 	sample = sorted(sample)
-	return sample
+	return sample, dfClusters
 
 args = getArguments()
 method=args.method
@@ -143,11 +270,12 @@ infile=args.infile
 outfile=args.outfile
 store = pd.HDFStore(args.infile,'r')
 
+dfClusters=None
 # printStore(store)
 if method.upper()=="KMEANS":
-	sample = KMeansClustering(store,args)
+	sample, dfClusters = KMeansClustering(store,args)
 if method.upper()=="DBSCAN" or  method.upper()=="HDBSCAN" :
-	sample = DBSCANClustering(store,args)
+	sample, dfClusters = DBSCANClustering(store,args)
 store.close()
 
 print("# of selected structures = ",len(sample))
@@ -157,11 +285,12 @@ if len(sample)<50:
 # to check
 dfn=pd.DataFrame(sample)
 dfn.to_csv(outfile,index=False)
-
-
 print("number of selected structures are saved in ", outfile, " file", flush=True)
 
 # to check
 #dfnt=pd.read_csv(outfile)
 #print(dfnt)
 
+if dfClusters is not None:
+	dfClusters.to_csv(args.outclustersfile)
+	print("Clusters informations are saved in ", args.outclustersfile, " file", flush=True)
