e3c4778a7be6905a45bb1e8184867860386d40f9
allouchear <77264549+allouchear@users.noreply.github.com>
2023-11-07 17:15:23 +0100

    more options in metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -12,6 +12,8 @@ def getArguments():
 	parser.add_argument("--outfile", type=str, default="metrics", help="prefix of outfiles : one .h file for all Z and one .csv for each Z")
 	parser.add_argument("--bins", type=int, default=100, help=" bins = number of bins to build the histograme (100 = default)")
 	parser.add_argument("--infiles", type=str, default="funct*h5", help="--infiles=list of file. Example --infiles=func*h5,a.h5")
+	parser.add_argument("--p", type=float, default=-10, help=" real : number of values/number of all bins(N Dimension)*100, default=-10. If p<0 : m=int(-p) bins for each direction")
+	parser.add_argument("--seed", type=int, default=111, help=" seed, default 111. If <0 => random_state=None")
 
 	args = parser.parse_args()
 
@@ -26,13 +28,75 @@ def getX2(dfc,bins):
 	nx2=x2/np.sum(h)
 	return x2, nx2
 
-def getMetricsOneZ(df, bins):
+def getChi2nDim(df, args):
+	"""Return Chi2 uzing all Z
+
+	Parameters
+	----------
+	df : dataframe containing G values for one Z
+	args
+	"""
+	ndf=df
+	n_components = df.shape[1]-1
+	nAll=ndf.shape[0]
+	if args.p>0:
+		m = int((nAll/100*args.p)**(1.0/n_components))
+		if m<2:
+			m = 2
+		print("histogram size for one variable=",m)
+		mall = m**n_components
+		print("histogram size=",mall)
+	else:
+		m = int(-args.p)
+		print("histogram size for one variable=",m)
+		mall = m**n_components
+		print("histogram size=",mall)
+	cols=[]
+	print("n_components =",n_components)
+	for ic in range(n_components):
+		xColName="G"+str(ic+1)
+		kColName="K"+str(ic+1)
+		xmin=df[xColName].min()
+		xmax=df[xColName].max()
+		dx = (xmax-xmin)/m;
+		#print("ic= {:0.12e} xmin= {:0.12e} xmax = {:0.12e} dx = {:0.12e}".format(ic+1,xmin,xmax,dx))
+		if abs(dx)>1e-14:
+			kAll = (df[xColName]-xmin)/dx;
+			df[kColName] = kAll
+			df[kColName] = df[kColName].astype('int')
+			cols.append(kColName)
+			df.loc[df[kColName] >=m, kColName] = m-1
+			#print(df[df[kColName]>=m])
+
+	df = df.groupby(by=cols).aggregate(['count'])['G1']
+	h = df.reset_index()["count"].values
+	nAll=np.sum(h)
+	aver=nAll/m**n_components
+	#s = aver*m**n_cmpounts # all cas are 0 
+	s = nAll # all cas are 0 
+	lh = len(h)
+	s -= lh*aver # for case with n>0, remove aver
+	haver= [aver]*lh
+	s += ((h-haver)**2/haver).sum()
+	chi2 = s/nAll
+	print("chi2=",chi2,'m=',m)
+	# now compute chi2 using only occuped sites
+	aver=nAll/lh
+	haver= [aver]*lh
+	rchi2 = ((h-haver)**2/haver).sum()/nAll
+	print("chi2 computed using occuped sites =",rchi2)
+		
+	return chi2, rchi2
+
+
+
+def getMetricsOneZ(df, args):
 	"""Return metrics for one Z
 
 	Parameters
 	----------
 	df : dataframe containing G values for one Z
-	bins : the number of bins used to build histogram
+	args : 
 
 	Returns
 	-------
@@ -48,7 +112,7 @@ def getMetricsOneZ(df, bins):
 	R = []
 	for c in df.columns:
 		#print(c)
-		x2, nx2 = getX2(df[c],bins)
+		x2, nx2 = getX2(df[c],args.bins)
 		x2l += [x2] 
 		nx2l += [nx2] 
 		R += [ df[c].max()-df[c].min() ]
@@ -65,8 +129,8 @@ def getMetricsOneZ(df, bins):
 	return dfMetrics
 
 def getMetrics(args,files):
-	bins=args.bins
 	dfMetrics = {}
+	dfChi2nDim = {}
 	for f in files:
 		store = pd.HDFStore(f,'r')
 		st = 'file name = {:s}'.format(f)
@@ -77,7 +141,7 @@ def getMetrics(args,files):
 		for key in store.keys(): 
 			print("Type = ",key)
 			df = store.get(key)
-			dfm = getMetricsOneZ(df, bins)
+			dfm = getMetricsOneZ(df, args)
 			dfm['file']=f
 			z=int(df.iloc[0,0])
 			sz=str(z)
@@ -85,9 +149,17 @@ def getMetrics(args,files):
 				dfMetrics[str(z)] = pd.concat([dfMetrics[str(z)], dfm])
 			else:
 				dfMetrics[str(z)] = dfm
+			dfc = pd.DataFrame({"file":[f]})
+			chi2, rchi2 = getChi2nDim(df, args)
+			dfc['Chi2nDim']=chi2
+			dfc['RChi2nDim']=rchi2
+			if sz in dfChi2nDim.keys():
+				dfChi2nDim[str(z)] = pd.concat([dfChi2nDim[str(z)],dfc])
+			else:
+				dfChi2nDim[str(z)] = dfc
 		store.close()
 		print("-----------------------------------------------------------",flush=True)
-	return dfMetrics
+	return dfMetrics,  dfChi2nDim
 
 def saveByMean(dfMetrics,args, key='nx2'):
 	soutfile=os.path.splitext(args.outfile)[0]+'_sorted'+'_'+key+'.txt'
@@ -103,6 +175,27 @@ def saveByMean(dfMetrics,args, key='nx2'):
 	print("Data sorted by ", key, " are saved in ", soutfile, " file", flush=True)
 	print("================================================================", flush=True)
 
+def saveByChi2nDim(dfChi2nDim,args):
+	key="Chi2nDim"
+	key2="RChi2nDim"
+	soutfile=os.path.splitext(args.outfile)[0]+'_sorted'+'_'+key+'.txt'
+	f=open(soutfile, 'w')
+	print(" =======================================================================",file=f)
+	print(" Chi2nDim   : Chi2 computed using all cells including those with 0 value",file=f)
+	print(" RChi2nDim  : Chi2 computed using lonly cells with 1 or more values",file=f)
+	print(" =======================================================================",file=f)
+	for sz in  dfChi2nDim.keys():
+		print("Z=",sz,file=f)
+		print("=======",file=f)
+		#print( dfChi2nDim[sz].reset_index()[[key,key2,'file']].drop_duplicates(subset='file', keep='first').sort_values(by=[key], ascending=True).to_string(),file=f)
+		print( dfChi2nDim[sz].reset_index()[[key,key2,'file']].drop_duplicates(subset='file', keep='first').sort_values(by=[key2], ascending=True).to_string(),file=f)
+		for i in range(140):
+			print("-", end='',file=f)
+		print(file=f)
+	f.close()
+	print("Data sorted by ", key, " are saved in ", soutfile, " file", flush=True)
+	print("=========================================================================", flush=True)
+
 args = getArguments()
 infiles=args.infiles
 infiles=infiles.split(',')
@@ -113,7 +206,7 @@ print(files)
 if len(files)<1:
 	print("Error : check --infiles parameters ")
 	sys.exit(1)
-dfMetrics = getMetrics(args,files)
+dfMetrics, dfChi2nDim = getMetrics(args,files)
 outfile=args.outfile 
 h5outfile=os.path.splitext(args.outfile)[0]+'.h5'
 storeOut = pd.HDFStore(h5outfile,"w")
@@ -141,3 +234,4 @@ saveByMean(dfMetrics,args, key='nx2')
 saveByMean(dfMetrics,args, key='R')
 saveByMean(dfMetrics,args, key='std')
 
+saveByChi2nDim(dfChi2nDim,args)
